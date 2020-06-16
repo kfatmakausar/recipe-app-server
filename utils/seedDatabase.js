e121828da50615ec63842b871be7734e772a9a55
Fatma Kausar <kfatmakausar@gmail.com>
2020-06-16 14:39:49 -0400

    Database done

--- utils/seedDatabase.js
+++ utils/seedDatabase.js
@@ -1,23 +1,42 @@
-// const { Player } = require("../database/models");
+const { Recipe, Review } = require("../database/models");
 
-// const seedDatabase = async () => {
-//   await Promise.all([
-//     Player.create({
-//       firstName: "Kyrie",
-//       lastName: "Irving",
-//       jerseyNumber: 11,
-//     }),
-//     Player.create({
-//       firstName: "LeBron",
-//       lastName: "James",
-//       jerseyNumber: 23,
-//     }),
-//     Player.create({
-//       firstName: "Luka",
-//       lastName: "Doncic",
-//       jerseyNumber: 77,
-//     }),
-//   ]);
-// };
 
-// module.exports = seedDatabase;
+const seedDatabase = async () => {
+  await Promise.all([
+    Recipe.create({
+      title: "Pizza",
+      readyInMinutes: "30",
+      servings: "4",
+      sourceURL: "https://www.simplyrecipes.com/recipes/homemade_pizza/",
+    }),
+    Recipe.create({
+        title: "Bagels",
+        readyInMinutes: "60",
+        servings: "4",
+        sourceURL: "https://sallysbakingaddiction.com/homemade-bagels/",
+    }),
+    Recipe.create({
+        title: "Alfredo Pasta",
+        readyInMinutes: "45",
+        servings: "4",
+        sourceURL: "https://www.thekitchn.com/how-to-make-chicken-alfredo-pasta-249767",
+    }),
+    Review.create({
+      userName: "kfatmakausar",
+      description: "Delicious pizza! Amazing recipe, must try!",
+      recipeId: 1
+    }),
+    Review.create({
+        userName: "alextho",
+        description: "Burnt pizza! Wrong oven time given!!",
+        recipeId: 1
+    }),
+    Review.create({
+        userName: "arslan",
+        description: "Yummy bagels!!",
+        recipeId: 2
+    }),
+  ]);
+};
+
+module.exports = seedDatabase;
